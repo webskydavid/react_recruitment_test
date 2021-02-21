e34d70665a0ed84340c89c04892c936a85905dba
webskydavid <webskydavid@gmail.com>
2021-02-21 17:57:27 +0100

    feat: Implement showing the list of products

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [cart, setCart] = useState([]);
+
+  const getCart = async () => {
+    const response = await fetch('api/cart');
+    const data = await response.json();
+    setCart(data);
+  };
+
+  useEffect(() => {
+    getCart();
+  }, []);
+
   return (
     <div className='container'>
       <h3>Lista produktów</h3>
       <ul>
-        <li data-testid='product' className='row'>
-          Patelnia, cena: 89,99zł
-        </li>
+        {cart.length
+          ? cart.map((product) => (
+              <li key={product.pid} data-testid='product' className='row'>
+                <span>
+                  {product.name}, cena: {product.price}
+                </span>
+                <div>+</div>
+                <div>-</div>
+              </li>
+            ))
+          : 'Pobieram listę'}
       </ul>
     </div>
   );
