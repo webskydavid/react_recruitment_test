15717c436ddc01deccad0e369b4f3fa74fe2267e
webskydavid <webskydavid@gmail.com>
2021-02-21 17:57:11 +0100

    test: Test for showing product on the list

--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -1,15 +1,60 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import {
+  cleanup,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../App/App';
+import { servicesVersion } from 'typescript';
 
-afterEach(() => cleanup);
+const mockData = [
+  {
+    pid: '8e5e1248-c799-4937-9acc-2b3ab0e034ff',
+    name: 'Patelnia',
+    price: '89.99',
+    max: 10,
+    min: 1,
+  },
+  {
+    pid: '993c12b2-e662-4af7-b0bc-fef5c1d47720',
+    name: 'Garnek mały',
+    price: '29.99',
+    max: 10,
+    min: 1,
+  },
+];
 
-test('should render list with products', () => {
-  const app = render(<App />);
+const server = setupServer(
+  rest.get('/api/cart', (req, res, ctx) => {
+    return res(ctx.json(mockData));
+  })
+);
 
-  expect(app.getByText(/lista produktów/i)).toBeInTheDocument();
-  expect(
-    app.getAllByTestId('product').map((prod) => prod.textContent)
-  ).toEqual(['Patelnia, cena: 89,99zł']);
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
+
+test('should render list with products', async () => {
+  render(<App />);
+
+  expect(screen.getByText(/lista produktów/i)).toBeInTheDocument();
+  expect(screen.getByText(/pobieram listę/i)).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(() => screen.getByText(/pobieram listę/i));
+
+  mockData.map((prod) => {
+    expect(
+      screen.getByText(`${prod.name}, cena: ${prod.price}`)
+    ).toBeInTheDocument();
+  });
+
+  screen.debug();
 });
