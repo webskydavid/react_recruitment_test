098cad84e48fb28c8f4675e8073f1f584031746c
webskydavid <webskydavid@gmail.com>
2021-02-22 18:08:02 +0100

    feat: Implement changing quantity

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const priceSum = (data, sum) =>
+  data
+    .map((prod) => Number.parseFloat(prod.price) * prod.quantity)
+    .reduce((prev, curr) => prev + curr, sum);
+
 const App = () => {
+  const [change, setChange] = useState(false);
   const [cart, setCart] = useState([]);
   const [sum, setSum] = useState(0);
 
   const getCart = async () => {
     const response = await fetch('api/cart');
     const data = await response.json();
-    const priceSum = data.reduce(
-      (prev, curr) => prev + Number.parseInt(curr.price),
-      sum
-    );
-    setCart(data);
-    setSum(priceSum);
+    const result = data.map((product) => ({ ...product, quantity: 1 }));
+    setCart(result);
+    setSum((s) => priceSum([...result], s));
+  };
+
+  const setQuantity = (type, pid) => {
+    setChange(true);
+    setCart((s) => [
+      ...s.map((prod) => {
+        if (prod.pid === pid) {
+          const quantity =
+            type === 'increment' ? prod.quantity + 1 : prod.quantity - 1;
+          return { ...prod, quantity };
+        }
+        return prod;
+      }),
+    ]);
   };
 
   useEffect(() => {
     getCart();
   }, []);
+
+  useEffect(() => {
+    if (cart.length > 0 && change) {
+      setSum(() => priceSum(cart, 0));
+    }
+  }, [cart, change]);
 
   return (
     <div className='container'>
@@ -31,9 +54,21 @@ const App = () => {
                   {product.name}, cena: {product.price.replace('.', ',')} zł
                 </span>
                 <div data-testid='quantity'>
-                  <button data-testid='decrement'>-</button>
-                  <button data-testid='increment'>+</button>
-                  <span data-testid='amount'>Obecnie masz 1 szt. produktu</span>
+                  <button
+                    data-testid='decrement'
+                    onClick={() => setQuantity('decrement', product.pid)}
+                  >
+                    -
+                  </button>
+                  <button
+                    data-testid='increment'
+                    onClick={() => setQuantity('increment', product.pid)}
+                  >
+                    +
+                  </button>
+                  <span data-testid='amount'>
+                    Obecnie masz {product.quantity} szt. produktu
+                  </span>
                 </div>
               </li>
             ))
