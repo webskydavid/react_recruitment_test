9ff212f52fda231b231fa5b73712edac8b947a34
webskydavid <webskydavid@gmail.com>
2021-02-22 14:13:56 +0100

    feat: Implement displaying price sum

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 const App = () => {
   const [cart, setCart] = useState([]);
+  const [sum, setSum] = useState(0);
 
   const getCart = async () => {
     const response = await fetch('api/cart');
     const data = await response.json();
+    const priceSum = data.reduce(
+      (prev, curr) => prev + Number.parseInt(curr.price),
+      sum
+    );
     setCart(data);
+    setSum(priceSum);
   };
 
   useEffect(() => {
@@ -24,12 +30,13 @@ const App = () => {
                 <span>
                   {product.name}, cena: {product.price.replace('.', ',')} zł
                 </span>
-                <div>+</div>
                 <div>-</div>
+                <div>+</div>
               </li>
             ))
           : 'Pobieram listę'}
       </ul>
+      <span data-testid='price_sum'>Suma zamówienia: {sum} zł</span>
     </div>
   );
 };
